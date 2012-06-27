fd79ec5a8f8bc9ca1c6595b394ed3790d6e82bd0
aikeru_cp <aikeru@users.noreply.github.com>
2012-06-27 22:03:43 +0000

    Partial support for 'require()' and 'exports' ie: http://stackoverflow.com/questions/5311334/what-is-the-purpose-of-nodejs-module-exports-and-how-do-you-use-it/5311377#5311377

---
+++ JuraScriptLibrary/Require/requireObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jurassic.Library;
+using Jurassic;
+
+namespace JuraScriptLibrary.Require
+{
+    public class requireObject : FunctionInstance
+    {
+        public requireObject(ObjectInstance prototype) : base(prototype)
+        {
+            this.PopulateFunctions();
+        }
+        public override object CallLateBound(object thisObject, params object[] argumentValues)
+        {
+            if (argumentValues != null
+                && argumentValues.Length > 0)
+            {
+                var mod = argumentValues[0];
+                if (mod.GetType() == typeof(String))
+                {
+                    //Find module
+
+                }
+                Console.WriteLine("TYPE OF ARGUMENT: " + mod.GetType());
+                Console.WriteLine(mod as ArrayInstance != null ? "It's an array" : "It's not an array");
+                Console.WriteLine(mod as StringInstance != null ? "It's a string" : "It's not a string");
+            }
+            return null;
+        }
+    }
+}
