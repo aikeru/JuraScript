888a0771d4a405a97d0f5c1460de818f0fc31c51
aikeru_cp <aikeru@users.noreply.github.com>
2012-06-28 20:53:18 +0000

    No parameter or /? and --? cause JuraScript to output usage text.

--- JuraScript/Program.cs
+++ JuraScript/Program.cs
@@ -14,8 +14,24 @@ namespace JuraScript
     {
         static void Main(string[] args)
         {
-            JuraScriptObject jso = new JuraScriptObject();
-            jso.StartUp(args);
+            if (args.Length == 0
+                || args[0] == "/?" || args[0] == "--?")
+            {
+                Console.WriteLine();
+                Console.WriteLine("JuraScript");
+                Console.WriteLine("(c) " + DateTime.Now.Year.ToString() + " Michael Snead");
+                Console.WriteLine();
+                Console.WriteLine("Usage: ");
+                Console.WriteLine();
+                Console.WriteLine(" jurascript /CON - Start JuraScript console/shell");
+                Console.WriteLine(" jurascript <filename> - Execute script <filename>");
+                Console.WriteLine();
+            }
+            else
+            {
+                JuraScriptObject jso = new JuraScriptObject();
+                jso.StartUp(args);
+            }
         }
     }
 }
